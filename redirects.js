6fe06c7be2e92439629504ef421b724e1bd08b62
William Oldham <wegg7250@gmail.com>
2024-02-26 17:51:52 +0000

    Setup quick links

---
+++ redirects.js
@@ -0,0 +1,12 @@
+window.redirects = {
+  "/weblate": {
+    url: "https://weblate.476328473.xyz",
+    name: "Weblate",
+    extra: "Contribute to translations",
+  },
+  "/discord": {
+    url: "https://discord.gg/gQYB6fGArX",
+    name: "Discord",
+    extra: "Join our community",
+  },
+};
